8e9ff6c7a6f5c9e117190ea59cd2a7d8c67bdc51
xingyu4j <xingyu4j@vip.qq.com>
2021-01-04 10:50:42 +0800

    【同步】github源代码到2021.01.01

--- xchange-binance/src/test/java/org/knowm/xchange/binance/BinanceAdaptersTest.java
+++ xchange-binance/src/test/java/org/knowm/xchange/binance/BinanceAdaptersTest.java
@@ -8,46 +8,54 @@ import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.trade.MarketOrder;
 import org.knowm.xchange.utils.ObjectMapperHelper;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BinanceAdaptersTest {
 
-  @Test
-  public void testFilledMarketOrder() throws Exception {
-    BinanceOrder binanceOrder =
-        ObjectMapperHelper.readValue(
-            BinanceAdaptersTest.class.getResource("/filled-market-order.json"), BinanceOrder.class);
-    Order order = BinanceAdapters.adaptOrder(binanceOrder);
-    assertThat(order).isInstanceOf(MarketOrder.class);
-    MarketOrder marketOrder = (MarketOrder) order;
-    assertThat(marketOrder.getStatus()).isEqualByComparingTo(Order.OrderStatus.FILLED);
-    assertThat(marketOrder.getOriginalAmount()).isEqualByComparingTo("0.10700000");
-    assertThat(marketOrder.getCumulativeAmount()).isEqualByComparingTo("0.10700000");
-    assertThat(marketOrder.getRemainingAmount()).isEqualByComparingTo(BigDecimal.ZERO);
-    assertThat(marketOrder.getAveragePrice()).isEqualByComparingTo("0.01858383");
-    assertThat(marketOrder.getOrderFlags())
-        .contains(BinanceOrderFlags.withClientId("gzcLIkn86ag3FycOCEl6Vi"));
+    @Test
+    public void testFilledMarketOrder() throws IOException {
 
-    MarketOrder copy = ObjectMapperHelper.viaJSON(marketOrder);
-    assertThat(copy).isEqualToComparingFieldByField(marketOrder);
-  }
+        //    InputStream is =
+        //        BinanceAdaptersTest.class.getResourceAsStream(
+        //            "/org/knowm/xchange/binance/filled-market-order.json");
+        //    // Use Jackson to parse it
+        //    ObjectMapper mapper = new ObjectMapper();
+        //    BinanceOrder binanceOrder = mapper.readValue(is, BinanceOrder.class);
+        BinanceOrder binanceOrder =
+                ObjectMapperHelper.readValue(
+                        BinanceAdaptersTest.class.getResource("/filled-market-order.json"), BinanceOrder.class);
+        Order order = BinanceAdapters.adaptOrder(binanceOrder);
+        assertThat(order).isInstanceOf(MarketOrder.class);
+        MarketOrder marketOrder = (MarketOrder) order;
+        assertThat(marketOrder.getStatus()).isEqualByComparingTo(Order.OrderStatus.FILLED);
+        assertThat(marketOrder.getOriginalAmount()).isEqualByComparingTo("0.10700000");
+        assertThat(marketOrder.getCumulativeAmount()).isEqualByComparingTo("0.10700000");
+        assertThat(marketOrder.getRemainingAmount()).isEqualByComparingTo(BigDecimal.ZERO);
+        assertThat(marketOrder.getAveragePrice()).isEqualByComparingTo("0.01858383");
+        assertThat(marketOrder.getOrderFlags())
+                .contains(BinanceOrderFlags.withClientId("gzcLIkn86ag3FycOCEl6Vi"));
 
-  @Test
-  public void testAssetDividendList() throws Exception {
-    AssetDividendResponse assetDividendList =
-        ObjectMapperHelper.readValue(
-            BinanceAdaptersTest.class.getResource("/asset-dividend-list.json"),
-            AssetDividendResponse.class);
+        MarketOrder copy = ObjectMapperHelper.viaJSON(marketOrder);
+        assertThat(copy).isEqualToComparingFieldByField(marketOrder);
+    }
 
-    assertThat(assetDividendList.getTotal()).isEqualByComparingTo(BigDecimal.ONE);
+    @Test
+    public void testAssetDividendList() throws Exception {
+        AssetDividendResponse assetDividendList =
+                ObjectMapperHelper.readValue(
+                        BinanceAdaptersTest.class.getResource("/asset-dividend-list.json"),
+                        AssetDividendResponse.class);
 
-    AssetDividendResponse.AssetDividend assetDividend = assetDividendList.getData().get(0);
-    assertThat(assetDividend.getAmount()).isEqualByComparingTo(BigDecimal.valueOf(10L));
-    assertThat(assetDividend.getAsset()).isEqualTo("BHFT");
-    assertThat(assetDividend.getDivTime()).isEqualByComparingTo(1563189166000L);
-    assertThat(assetDividend.getEnInfo()).isEqualTo("BHFT distribution");
-    assertThat(assetDividend.getTranId()).isEqualByComparingTo(2968885920L);
-  }
+        assertThat(assetDividendList.getTotal()).isEqualByComparingTo(BigDecimal.ONE);
+
+        AssetDividendResponse.AssetDividend assetDividend = assetDividendList.getData().get(0);
+        assertThat(assetDividend.getAmount()).isEqualByComparingTo(BigDecimal.valueOf(10L));
+        assertThat(assetDividend.getAsset()).isEqualTo("BHFT");
+        assertThat(assetDividend.getDivTime()).isEqualByComparingTo(1563189166000L);
+        assertThat(assetDividend.getEnInfo()).isEqualTo("BHFT distribution");
+        assertThat(assetDividend.getTranId()).isEqualByComparingTo(2968885920L);
+    }
 }
